43b01ff2d420b5918a078e87fb1d4b710bf57205
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-09-07 17:34:18 -0300

    Projeto iniciado

---
+++ src/test/java/com/event/looper/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.looper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
