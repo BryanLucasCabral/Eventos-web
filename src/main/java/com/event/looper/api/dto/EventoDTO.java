83c1eb9c8927b56d2494da8750be2ef32aa3e372
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-09-10 20:51:06 -0300

    EVT-006 Criado a camada DTO da entidade Evento

---
+++ src/main/java/com/event/looper/api/dto/EventoDTO.java
@@ -0,0 +1,21 @@
+package com.event.looper.api.dto;
+
+import java.time.LocalDate;
+
+import com.event.looper.api.model.Organizador;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventoDTO {
+    private String titulo;
+    private String detalhes;
+    private String slug;
+    private LocalDate dataEvento;
+    private Integer limiteParticipantes;
+    private Organizador organizadorId;
+}
