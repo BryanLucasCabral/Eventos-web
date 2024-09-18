9439ecf393bebd514071bdfe6e0df6c4083a138d
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-09-17 21:55:47 -0300

    EVT-007 fix nas entidades

--- src/main/java/com/event/looper/api/model/Evento.java
+++ src/main/java/com/event/looper/api/model/Evento.java
@@ -1,17 +1,24 @@
 package com.event.looper.api.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.event.looper.api.dto.EventoDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -39,11 +46,20 @@ public class Evento {
     @Column(nullable = false)
     private LocalDate dataEvento;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "EVT_ORGANIZADORES_ID", referencedColumnName = "id")
-    private Organizador organizadorId;
+    private Organizador organizador;
 
-    @Column(nullable = false, name = "LIMITE_PARTICIPANTES")
+    @ManyToMany
+    @JoinTable( 
+        name = "EVT_EVENTOS_PARTICIPANTES",
+        joinColumns = @JoinColumn(name = "evento_id"),
+        inverseJoinColumns = @JoinColumn(name = "participante_id")
+    )
+    private List<Participante> participantes = new ArrayList<>();
+
+    @Min(1)
+    @Column(nullable = false)
     private Integer limiteParticipantes;
 
     public EventoDTO toDto(){
--- src/main/java/com/event/looper/api/model/Organizador.java
+++ src/main/java/com/event/looper/api/model/Organizador.java
@@ -3,13 +3,16 @@ package com.event.looper.api.model;
 
 import com.event.looper.api.dto.OrganizadorDTO;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,10 +27,11 @@ public class Organizador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(nullable = false)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id",nullable = false)
     private Usuario usuario;
 
+    @Pattern(regexp = "\\d{14}", message = "CNPJ deve conter 14 dígitos numéricos")
     @Column(nullable = false,length = 14, unique = true)
     private String cnpj;
 
@@ -39,7 +43,7 @@ public class Organizador {
 
         dto.setCnpj(cnpj);
         dto.setRazaoSocial(razaoSocial);
-        dto.setUsuario(usuario);
+        dto.setUsuario(usuario.toDto());
 
         return dto;
     }
